ade5ec8b08502b98527abd45567c5b0bb19fbbec
Matthew Collins <matthew.collins@defra.gov.uk>
2019-06-17 11:42:38 +0100

    Updates for content-tagger

--- giant-docker/setupimage-7.sh
+++ giant-docker/setupimage-7.sh
@@ -2,6 +2,9 @@
 
 cd ~
 source ~/.rbenv_setup
-cd /var/govuk/govuk-puppet
-puppet module install puppet-zypprepo --modulepath /var/govuk/govuk-puppet/modules:/var/govuk/govuk-puppet/vendor/modules
-puppet module install puppet-python --version=1.19.0 --modulepath /var/govuk/govuk-puppet/modules:/var/govuk/govuk-puppet/vendor/modules
+cd /var/govuk/govuk-guix
+sudo apt-get install wget
+sudo gpg --keyserver pool.sks-keyservers.net --recv-keys 3CE464558A84FDC69DB40CFB090B11993D9AEBB5
+#wget https://git.savannah.gnu.org/cgit/guix.git/plain/etc/guix-install.sh
+echo y | sudo /home/govuk/guix-install.sh
+exit 0
